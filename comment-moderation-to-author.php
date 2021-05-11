5d3a2ece9d711d90d70f147615799a5354737c30
RavanH <ravanhagen@gmail.com>
2021-05-11 16:34:01 +0200

    v0.6 allow other filters running earlier

--- comment-moderation-to-author.php
+++ comment-moderation-to-author.php
@@ -3,7 +3,7 @@
 Plugin Name: Comment Moderation E-mail only to Author
 Plugin URI: http://status301.net/wordpress-plugins/comment-moderation-e-mail-to-post-author/
 Description: Send comment moderation notifications **only** to the posts Author, not to the site Administration address (as configured on Settings > General) any more, unless the author in question has no moderation rights. There are no options, just activate and the site admin will no longer be bothered with notifications about posts from other authors. <strong>Happy with it? <em><a href="https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=ravanhagen%40gmail%2ecom&item_name=Comment%20Moderation%20E-mail%20to%20Post%20Author&item_number=0%2e4&no_shipping=0&tax=0&bn=PP%2dDonationsBF&charset=UTF%2d8&lc=us">Buy me a coffee...</a></em> Thanks! :)</strong>
-Version: 0.5
+Version: 0.6
 Author: RavanH
 Author URI: http://status301.net/
 License: GNU General Public License v3 or later
@@ -20,15 +20,22 @@ License URI: http://www.gnu.org/licenses/gpl-3.0.html
  * @param int   $comment_id Comment ID.
  * @return array
  */
-function comment_moderation_post_author_only($emails, $comment_id)
+function comment_moderation_post_author_only( $emails, $comment_id )
 {
-	$comment = get_comment($comment_id);
-	$post = get_post($comment->comment_post_ID);
-	$user = get_userdata($post->post_author);
-
-	// Return only the post author if the author can modify.
-	if ( user_can($user->ID, 'edit_comment', $comment_id) && !empty($user->user_email) )
-		return array( $user->user_email );
+	// Do we have multiple recipients at all?
+	if ( is_array( $emails ) && count( $emails ) > 1 ) {
+		// Most likely, the first element is the admin email and the second is the post author email.
+		// But another filter might be responsible for additional emails so...
+		$admin_email = get_option( 'admin_email' );
+		$comment = get_comment( $comment_id );
+		$post = get_post( $comment->comment_post_ID );
+		$user = get_userdata( $post->post_author );
+		
+		// Make extra sure the admin email is NOT the same as the original post author email before removing it!
+		if ( ! empty( $user->user_email ) && 0 !== strcasecmp( $user->user_email, $admin_email ) ) {
+			$emails = array_diff( $emails, array( $admin_email ) );
+		}
+	}
 
 	return $emails;
 }
